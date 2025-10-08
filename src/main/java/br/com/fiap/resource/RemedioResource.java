7f966db9a5838b85da6b8abc7a381e3d81cdaca7
PxS00 <lucas.rossonidd@gmail.com>
2025-10-08 10:11:26 -0300

    feat: adiciona método POST para salvar medicamentos com resposta adequada

--- src/main/java/br/com/fiap/resource/RemedioResource.java
+++ src/main/java/br/com/fiap/resource/RemedioResource.java
@@ -1,10 +1,8 @@
 package br.com.fiap.resource;
 
-import br.com.fiap.BO.RemedioBO;
+import br.com.fiap.bo.RemedioBO;
 import br.com.fiap.to.RemedioTO;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -26,5 +24,20 @@ public class RemedioResource {
         }
         response.entity(resultado);
         return response.build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response save(RemedioTO remedio) {
+        RemedioTO resultado = remedioBO.save(remedio);
+        Response.ResponseBuilder response = null;
+        if (resultado != null) {
+            response = Response.created(null); // 201 CREATED
+        } else {
+            response = Response.status(404); // 404 Not Found
+        }
+        response.entity(resultado);
+        return response.build();
+
     }
 }
