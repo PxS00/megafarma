e032b58ca207cf97c0bdcd42d5f03fec91fa1c37
PxS00 <lucas.rossonidd@gmail.com>
2025-10-10 14:10:09 -0300

    feat: implementa métodos para buscar e salvar medicamentos no banco de dados

--- src/main/java/br/com/fiap/dao/RemedioDAO.java
+++ src/main/java/br/com/fiap/dao/RemedioDAO.java
@@ -4,38 +4,81 @@ import br.com.fiap.to.RemedioTO;
 
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class RemedioDAO {
-    public ArrayList<br.com.fiap.to.RemedioTO> findAll(){
-        ArrayList<br.com.fiap.to.RemedioTO>remedios = new ArrayList<>();
-        br.com.fiap.to.RemedioTO remedio = new br.com.fiap.to.RemedioTO(1L, "Loratadina", 7.93, LocalDate.parse("2023-10-10"), LocalDate.parse("2025-10-10"));
+    public ArrayList<RemedioTO> findAll() {
+        ArrayList<RemedioTO> remedios = new ArrayList<>();
+        String sql = "select * from ddd_remedios order by codigo";
+        try (PreparedStatement ps = ConnectionFactory.getConnection().prepareStatement(sql)) {
+            ResultSet rs = ps.executeQuery();
+            if (rs!=null){
+                while (rs.next()){
+                    RemedioTO remedio = new RemedioTO();
+                    remedio.setCodigo(rs.getLong("codigo"));
+                    remedio.setNome(rs.getString("nome"));
+                    remedio.setPreco(rs.getDouble("preco"));
+                    remedio.setDataDeFabricacao(rs.getDate("data_de_fabricacao").toLocalDate());
+                    remedio.setDataDeValidade(rs.getDate("data_de_validade").toLocalDate());
 
-        remedios.add(remedio);
+                    remedios.add(remedio);
+                }
 
-        remedio = new br.com.fiap.to.RemedioTO(2L, "Amoxicilina", 26.50, LocalDate.now(), LocalDate.now().plusYears(2));
-        remedios.add(remedio);
+            } else {
+                return null;
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Erro na Consulta: " + e.getMessage());
+        } finally {
+            ConnectionFactory.closeConnection();
+        }
+
         return remedios;
-    };
 
-    public RemedioTO save(RemedioTO remedio){
+    }
+
+    public RemedioTO findByCodigo(long codigo){
+        RemedioTO remedio = new RemedioTO();
+        String sql = "select * from ddd_remedios where codigo = ?";
+
+        try (PreparedStatement ps = ConnectionFactory.getConnection().prepareStatement(sql)) {
+            ps.setLong(1, codigo);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                remedio.setCodigo(rs.getLong("codigo"));
+                remedio.setNome(rs.getString("nome"));
+                remedio.setPreco(rs.getDouble("preco"));
+                remedio.setDataDeFabricacao(rs.getDate("data_de_fabricacao").toLocalDate());
+                remedio.setDataDeValidade(rs.getDate("data_de_validade").toLocalDate());
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            System.out.println("Erro na Consulta: " + e.getMessage());
+        } finally {
+            ConnectionFactory.closeConnection();
+        }
+        return remedio;
+    }
+
+    public RemedioTO save(RemedioTO remedio) {
         String sql = "insert into ddd_remedios(nome, preco, data_de_fabricacao, data_de_validade) values(?,?,?,?)";
-        try(PreparedStatement ps = ConnectionFactory.getConnection().prepareStatement(sql))
-        {
+        try (PreparedStatement ps = ConnectionFactory.getConnection().prepareStatement(sql)) {
             ps.setString(1, remedio.getNome());
             ps.setDouble(2, remedio.getPreco());
             ps.setDate(3, Date.valueOf(remedio.getDataDeFabricacao()));
             ps.setDate(4, Date.valueOf(remedio.getDataDeValidade()));
-            if (ps.executeUpdate() > 0){
+            if (ps.executeUpdate() > 0) {
                 return remedio;
-            }else {
+            } else {
                 return null;
             }
         } catch (SQLException e) {
             System.out.println("Erro ao Salvar: " + e.getMessage());
-        }finally {
+        } finally {
             ConnectionFactory.closeConnection();
         }
         return null;
